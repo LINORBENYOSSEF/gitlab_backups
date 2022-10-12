478436a42b4cddbe985cf2d80ae65126a74fb4ca
LinorBenYossef <49592750+LINORBENYOSSEF@users.noreply.github.com>
2022-10-12 22:34:54 +0300

    Create gitlab_backups.py

---
+++ gitlab_backups.py
@@ -0,0 +1,29 @@
+"""
+    @author: Linor Ben Yossef
+    @description: Backups of repository from Gitlab
+"""
+
+from datetime import datetime
+
+from airflow import DAG
+from airflow.models import Variable
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id='gitlab_backups',
+    description=' backups of gitlab',
+    schedule_interval="@weekly",
+    start_date=datetime(2022, 1, 1),
+    catchup=False,
+    tags=['gitlab', 'backup'],
+    default_args= {
+      'owner': 'Linor Ben Yossef'
+    },
+
+)as dag:
+    config = Variable.get('gitlab_backups'),
+    run_this = BashOperator(
+    task_id = 'gitlab_backups',
+    bash_command = r'path_to_file/gitlab_backup.sh --config ',
+    dag = dag,    
+    )
